290cf1a6bf88d80c3545e4a94fbb91a9f415327b
Ellen <evwhite2@gmail.com>
2020-04-14 18:25:43 -0500

    home page moved to Home.JS, App.JS cleaned up

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,41 @@
-import React from "react";
-import Table from "../components/Table"
+import React, { Component } from "react";
 
-function Home() {
+import employees from  "../employees.json";
+import Wrapper from "../components/Wrapper"
+import Row from "../components/Row";
+import TableHead from "../components/TableHead";
+
+class Home extends Component {
+  state = {
+    employees
+  };
+
+  render() {
   return (
-    <div>
+    <Wrapper>
+
       <h1>Employee Directory</h1>
-        <Table/>
-    </div>
+      <table className="table">
+
+        <TableHead/>
+        {this.state.employees.map(employee=>(
+          <Row 
+          key={employee.ID}
+          ID= {employee.ID}
+          first_name={employee.first_name}
+          last_name={employee.last_name}
+          email={employee.email}
+          title= {employee.Title}
+          />
+        ))}
+
+      </table>
+        
+    </Wrapper>
   );
 }
+
+}
+
 
 export default Home;
